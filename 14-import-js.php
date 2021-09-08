ccdf212ad26347f562bb2865fdabb729ac8081a1
Jess2403 <Web2020.Jessica@gmail.com>
2021-09-08 14:41:51 +0200

    test

--- 14-import-js.php
+++ 14-import-js.php
@@ -6,6 +6,41 @@ $jsonMeteo = json_decode($takeMeteo,true);
 
 // var_dump($jsonMeteo);
 
+foreach ($associativeArray as $valeur2) {
+    echo "<p>$</p>";
+}
+echo "<hr>";
+// récupère les clefs et les valeurs
+foreach ($associativeArray as $clef => $valeur2) {
+    echo "<p>$clef -> $valeur2</p>";
+
+if(isset($_GET['jsonMeteo'])){
+    
+
+
+    switch($_GET['jsonMeteo']){
+        case 1 : 
+            $title = $pages[1]["titre"];
+            $texte = $pages[1]["texte"];
+            break;
+        case 2: 
+            $title = $pages[2]["titre"];
+            $texte = $pages[2]["texte"];
+            break;
+        case 3: 
+            $title = $pages[3]["titre"];
+            $texte = $pages[3]["texte"];
+            break;
+        default : 
+            $title = "Erreur 404";
+            $texte = "Touche pas mon URL !!!";
+    }
+
+ 
+}else{
+    $title = $pages[1]["titre"];
+    $texte = $pages[1]["texte"];
+}
 
 ?>
 <!DOCTYPE html>
